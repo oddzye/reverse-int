a22ef38ab704d0c5957f65ec0d4ab7be0bd8b32a
oddzye <tzariov.dmitry@yandex.ru>
2020-02-24 23:25:38 +0300

    task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
-}
+    let str = n.toString();
+    if (n < 0) {
+        str = str.slice(1, str.length);
+    }
+    return str.split('').reverse().join(''); 
+};
